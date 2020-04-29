140c647ff52527455d92b65e72884baa5d50e3a4
Eliyahu Duvidzon <eliduvid1@gmail.com>
2020-04-29 12:41:08 +0300

    console graphics optimization

--- src/main/kotlin/me/eliduvid/mineSolver/Main.kt
+++ src/main/kotlin/me/eliduvid/mineSolver/Main.kt
@@ -3,19 +3,24 @@ package me.eliduvid.mineSolver
 import me.eliduvid.mineSolver.model.CellStatus
 import java.lang.Thread.sleep
 
+const val SPACES = "                                                                "
 
 fun main() {
     val (game, startCoordinates) = generateBasicGame(30, 16, 100)
+    clearScreen()
     solve(game, startCoordinates) { action ->
-        clearScreen()
+        moveToTop()
         println(game)
-        println(action)
+        println(action + SPACES)
         sleep(10)
     }
-    println("start $startCoordinates, " +
+    print("start $startCoordinates, " +
             "bombs marked ${game.allCells.count { it.status == CellStatus.MARKED }}/${game.bombs}, " +
             "closed cells ${game.allCells.count { it.status == CellStatus.CLOSED }}")
+}
 
+private fun moveToTop() {
+    print("\u001B[0;0H")
 }
 
 private fun clearScreen() {
